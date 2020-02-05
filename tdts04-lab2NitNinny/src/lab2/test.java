dfff08e7cb2b670f9aa109e7e3178cd3bfe4e454
Unknown <j-m.lasnier@hotmail.com>
2020-02-05 19:24:15 +0100

    very basic testing socket framework

--- tdts04-lab2NitNinny/src/lab2/test.java
+++ tdts04-lab2NitNinny/src/lab2/test.java
@@ -1,5 +1,21 @@
 package lab2;
 
-public class test {
+import java.net.ServerSocket;
+import java.net.Socket;
 
+public class test {
+	public static void main(String[] args) {
+
+		try {
+			System.out.println("waiting for client...");
+			ServerSocket ss = new ServerSocket(9806);
+			Socket soc = ss.accept();
+			System.out.println("Connection established");
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+
+	}
+	
 }
