99c3c190539ede65160981960765590cc9fc1e1c
Unknown <j-m.lasnier@hotmail.com>
2020-02-19 15:04:05 +0100

    added blacklist check and redirection

--- tdts04-lab2NitNinny/src/lab2/NetNinny.java
+++ tdts04-lab2NitNinny/src/lab2/NetNinny.java
@@ -1,15 +1,25 @@
 package lab2;
-import java.net.*;
+import java.net.*; 
 import java.io.*;
+import java.util.*;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 
 //http://requestbin.net/r/1gtru6d1
 //http://iberianodonataucm.myspecies.info/
 //http://diptera.myspecies.info/
+//https://www.ida.liu.se/~TDTS04/labs/2011/ass2/goodtest1.txt
+//https://www.ida.liu.se/~TDTS04/labs/2011/ass2/goodtest2.html
+//https://www.ida.liu.se/~TDTS04/labs/2011/ass2/SpongeBob.html
+//https://www.ida.liu.se/~TDTS04/labs/2011/ass2/badtest1.html
 
 public class NetNinny {
 	final static int BUFFER_SIZE = 65536;
 	final static int PORT = 80;
-	final static String[] blackList = {"odonata"};
+    final static String ERROR_PAGE = "HTTP/1.1 301 Moved Permanently\nContent-Length: 145\nLocation: http://zebroid.ida.liu.se/error1.html\nConnection: keep-alive\nContent-Type: text/html";
+	final static String[] blackList = {"odonata", "SpongeBob"};
+	
+	boolean legalRequest = true;
 	
 	
 	public static void main(String[] args){
@@ -35,9 +45,31 @@ public class NetNinny {
 					InputStream input = socketServer.getInputStream();
 					input.read(requestBuffer);
 					String inputRequest = new String(requestBuffer);
+					String header = inputRequest.split("\n")[0];
+					
+//					System.out.println("input: ");
+//					System.out.println(input);
+////					
+//					System.out.println("inputRequest: ");
+//					System.out.println(inputRequest);
+//					
+//					System.out.println("header: ");
+//					System.out.println(header);
+//					
+					//if illegal url, redirect and send error page to browser
+					if (isBlackListed(header)) {
+                        String redirect = getErrorPage();
+                        System.out.println(new String(redirect.getBytes()));
 
+                        OutputStream clientOutputStream = socketServer.getOutputStream();
+                        clientOutputStream.write(redirect.getBytes());
+                        socketServer.close();
+                        return;  
+					}
+					
 					//string manip to get port number and host name
-					String host = getHostName(inputRequest.split("\n")[0]);				
+					
+					String host = getHostName(header);				
 					
 					//send request to web server
 					Socket socketClient = new Socket(host, PORT);
@@ -49,14 +81,14 @@ public class NetNinny {
 					
 					InputStream inputWebResponse = socketClient.getInputStream();
 					inputWebResponse.read(responseBuffer);
-					String inputResponse = new String(requestBuffer);
+					String inputResponse = new String(responseBuffer);
 
-					System.out.println("RESPONSE:");
-					System.out.println(inputResponse);
+//					System.out.println("RESPONSE:");
+//					System.out.println(inputResponse);
 					
 					//filter
-					filterResponse(inputResponse);
 					
+						
 					//send response to browser
 					OutputStream serverOutputStream = socketServer.getOutputStream();
 					serverOutputStream.write(responseBuffer);
@@ -85,12 +117,21 @@ public class NetNinny {
 		return host;
 	}
 	
-	public static void filterResponse(String response) {
-		
+	public static boolean isBlackListed(String request ) {
+		for (String badWord: blackList) {
+			if (request.contains(badWord)) return true;
+		}
+		return false;
 	}
 	
-	public static boolean isBlackList(String word) {
-		return true;
+	public static String getErrorPage() {
+
+        DateFormat dateFormat = new SimpleDateFormat("E, dd MMM yyyy HH:mm:ss");
+        Date date = new Date();
+        String dateText = dateFormat.format(date) + " GMT\n";
+        dateText = dateText.replace(".", "");
+        return ERROR_PAGE + "\nDate: " + dateText;
 	}
-	
+
+
 }
